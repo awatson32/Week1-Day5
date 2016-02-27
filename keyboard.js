f1b0ce5bd1d9f836ec3a4d61ce5eff0353dee725
awatson32 <audrey.watson32@gmail.com>
2016-02-27 15:15:19 -0600

    Testing game functionality

--- keyboard.js
+++ keyboard.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
     var sentences = ['ten ate neite ate nee enet ite ate inet ent eate', 'Too ato too nOt enot one totA not anot tOO aNot', 'oat itain oat tain nate eate tea anne inant nean', 'itant eate anot eat nato inate eat anot tain eat', 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
-    //var letter = sentences[];
+    var lines = [];
+    var lineCount = 0;
+    $(sentences).each(function() {
+        var letters = this.split('');
+        var line = $('<div class="letters"></div>');
+		$(letters).each(function() {
+			var letter = this;
+			if (letter == ' ') {
+				letter = '&nbsp;';
+			}
+			line.append($('<div class="letter"> </div>').html(letter));
+		});
+		lines.push(line);
+	});
+    
+    $('#words').append(lines[lineCount]);
     
     //hide the keyboard-upper-container on load
     $('#keyboard-upper-container').hide();
@@ -24,11 +39,21 @@ $(document).ready(function() {
     });
     
     $(document).keypress(function(event) {
-        var key = event.charCode || event.keyCode;
-        var text = String.fromCharCode(key);       
-        $('#' + key).addClass('highlight');
-        if (text != '') {
-            $('#' + text).addClass('highlight');
+        var divKey = event.charCode || event.keyCode;        
+        var divTextChar = String.fromCharCode(divKey);
+        var divText = $(".letter").first().text();
+          
+        event.preventDefault();
+        
+        if (divKey === divText || (divText === '' && divKey === 160)) {
+            $('<span class="glyphicon glyphicon-ok"> </span>').appendTo('#words-typed');
+        } else {
+            $('<span class="glyphicon glyphicon-remove"> </span>').appendTo("#words-typed");
+        }
+        $(".letter").first().addClass("highlight-box remove-box").removeClass("letter");    
+        $('#' + divKey).addClass('highlight');
+        if (divText != '') {
+            $('#' + divText).addClass('highlight');
         };
         
          
